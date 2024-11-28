7c50c68c60010553240e733f4de1d6ac2c86d8fe
Mason Veilleux <masonjveilleux@gmail.com>
2024-11-27 22:33:26 -0600

    avoiding linting in next.config.ts

--- personal-website/next.config.js
+++ personal-website/next.config.js
@@ -6,7 +6,12 @@ import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
 const config = {
-    reactStrictMode: false
+    reactStrictMode: false,
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+      },
 };
 
 
