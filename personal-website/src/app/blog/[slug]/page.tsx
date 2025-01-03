d916536165aeb7a5378d5ef94b0a44652b74f2e3
Mason Veilleux <masonjveilleux@gmail.com>
2025-01-02 20:56:46 -0800

    fixing deploy issue with vercel

--- personal-website/src/app/blog/[slug]/page.tsx
+++ personal-website/src/app/blog/[slug]/page.tsx
@@ -1,19 +1,20 @@
 // app/blog/[slug]/page.tsx
-
 interface PageProps {
-    params: {
+    params: Promise<{
       slug: string;
-    }
+    }>;
   }
   
-  export default function BlogPost({ params }: PageProps) {
+  export default async function BlogPost({ params }: PageProps) {
+    const { slug } = await params;
+    
     return (
       <div className="w-full h-screen">
         <iframe
-          src={`/blogs/${params.slug}.html`}
+          src={`/blogs/${slug}.html`}
           className="w-full h-full border-none"
           title="Blog post content"
-          style={{ 
+          style={{
             minHeight: '100vh',
             width: '100%',
             margin: 0,
